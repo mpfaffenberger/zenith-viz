bfec2187b00ef5815343d367ef3edecce8ecc1a6
m0p0cnb <michael.pfaffenberger@walmart.com>
2024-06-17 10:21:32 -0400

    Fix OSX compiler issue, also fix build dependency issue

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from typing import List
 
-from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup, find_namespace_packages
-from setuptools.command.build_ext import build_ext
+from setuptools.command.build_ext import build_ext, Extension
 import sys
 import setuptools
 import pathlib
 import itertools
 import os
 
-os.environ["CXXSTD"] = "c++17"
-os.environ["CXX_STANDARD"] = "c++17"
 
 setuptools.distutils.log.set_verbosity(1)
 __version__ = "0.0.7"
@@ -35,7 +32,6 @@ def filter_cpp_or_c_files(directory: pathlib.Path) -> List[pathlib.Path]:
         file for file in files if file.name.endswith(".c") or file.name.endswith(".cpp") or (sys.platform == "darwin" and file.name.endswith(".m"))
     ]
     return files
-
 
 project_dirs = [
     "zenith_viz/cpp/glad/src",
@@ -79,9 +75,8 @@ zenith_viz_include_dirs = [
     fq_path("zenith_viz/cpp/imgui"),
     str(get_pybind_include()),
 ]
-print(zenith_viz_include_dirs)
 ext_modules = [
-    Pybind11Extension(
+    Extension(
         "_zenith",
         zenith_viz_srcs_with_deps,
         include_dirs=zenith_viz_include_dirs,
@@ -113,19 +108,6 @@ def has_flag(compiler, flagname):
     return True
 
 
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14/17] compiler flag.
-
-    The newer version is prefered over c++11 (when it is available).
-    """
-    flags = ["-std=c++17"]
-    for flag in flags:
-        if has_flag(compiler, flag):
-            return flag
-
-    raise RuntimeError("Unsupported compiler -- at least C++11 support " "is needed!")
-
-
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
 
@@ -141,7 +123,7 @@ class BuildExt(build_ext):
     if sys.platform == "darwin":
         frameworks = ["-framework Cocoa", "-framework IOKit", "-framework Foundation", "-framework QuartzCore", "-framework OpenGL"]
         os.environ["LDFLAGS"] = " ".join(frameworks)
-        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
+        darwin_opts = ["-stdlib=libc++", "-mmacosx-version-min=10.7", "-std=c++17"]
         c_opts["unix"] += darwin_opts
         l_opts["unix"] += darwin_opts
 
@@ -152,6 +134,16 @@ class BuildExt(build_ext):
         if sys.platform == "linux" or sys.platform == "linux2":
             opts.append("-D_GLFW_X11")
         elif sys.platform == "darwin":
+            original_compile_func = self.compiler._compile
+
+            def monkey_patched_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+                if src.endswith(".c") or src.endswith(".m"):
+                    cc_args = [cc_arg for cc_arg in cc_args if cc_arg != "-std=c++17"]
+                    extra_postargs = [ep for ep in extra_postargs if ep != "-std=c++17"]
+                return original_compile_func(obj, src, ext, cc_args, extra_postargs, pp_opts)
+
+            self.compiler._compile = monkey_patched_compile
+
             opts.append("-D_GLFW_COCOA")
         elif sys.platform == "win32":
             opts.append("-D_GLFW_WIN32")
@@ -174,7 +166,6 @@ class BuildExt(build_ext):
         self.force = True
         build_ext.build_extensions(self)
 
-
 setup(
     name="zenith_viz",
     version=__version__,
@@ -193,10 +184,6 @@ setup(
     },
     include_package_data=True,
     ext_modules=ext_modules,
-    setup_requires=[
-        "pybind11>=2.5.0",
-        "setuptools>=41",
-    ],
     cmdclass={"build_ext": BuildExt},
     zip_safe=False,
 )
